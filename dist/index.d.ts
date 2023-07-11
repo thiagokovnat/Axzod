872c8ba11d88fdd8b39b31c6c0e7abf225c83a70
Thiago Kovnat <thiago.kovnat@creativecoefficient.net>
2023-07-11 09:31:47 -0300

    fix declarations

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,10 +2,11 @@ import { AxiosInstance, AxiosRequestConfig, CreateAxiosDefaults } from "axios";
 import { z } from "zod";
 export interface AxzodInstance {
     instance: AxiosInstance;
-    get<T>(url: string, schema: z.ZodType<T>, headers?: {}): Promise<T>;
-    delete<T>(url: string, schema: z.ZodType<T>, headers?: {}): Promise<T>;
-    post<T>(url: string, schema: z.ZodType<T>, body?: {}, headers?: {}): Promise<T>;
-    put<T>(url: string, schema: z.ZodType<T>, body?: {}, headers?: {}): Promise<T>;
+    get<T>(url: string, schema: z.ZodType<T>, config?: AxzodRequestConfig): Promise<T>;
+    delete<T>(url: string, schema: z.ZodType<T>, config?: AxzodRequestConfig): Promise<T>;
+    post<T>(url: string, schema: z.ZodType<T>, body?: {}, config?: AxzodRequestConfig): Promise<T>;
+    put<T>(url: string, schema: z.ZodType<T>, body?: {}, config?: AxzodRequestConfig): Promise<T>;
+    request<T>(config: AxzodRequestConfig, schema: z.ZodType<T>): Promise<T>;
 }
 export interface CreateAxzodDefaults extends CreateAxiosDefaults {
     logging?: boolean;
